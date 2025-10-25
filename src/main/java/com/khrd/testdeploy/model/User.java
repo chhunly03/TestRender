ed04edbc3060e0c720f1838d65ed3623ee6a004f
Chhunly <chhunly03@example.com>
2025-10-26 01:02:57 +0700

    done

---
+++ src/main/java/com/khrd/testdeploy/model/User.java
@@ -0,0 +1,24 @@
+package com.khrd.testdeploy.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+}
